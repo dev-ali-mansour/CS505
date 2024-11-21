fe3586a5afa054206eb88a0497b2c63cb594fded
Ali Mansour <dev.ali.mansour@gmail.com>
2024-11-21 05:57:21 +0200

    refactor: Create Solution For Assignment 2 Find Next Greater Element Of Each Item In An Array Of Integers.

---
+++ assignments/2_next_greater_element/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void findNextGreaterElement(const int arr[], int size);
+
+int main() {
+    int arr[] = {13, 7, 8,6,12};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    findNextGreaterElement(arr, arrSize);
+
+    return 0;
+}
+
+void findNextGreaterElement(const int arr[], int size) {
+    int results[size];
+    stack<int> stk;
+
+    for (int i = size - 1; i >= 0; --i) {
+        while (!stk.empty() && stk.top() <= arr[i])
+            stk.pop();
+
+        results[i] = stk.empty() ? -1 : stk.top();
+
+        stk.push(arr[i]);
+    }
+
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " -> " << results[i] << endl;
+    }
+}
---
+++ assignments/2_next_greater_element/requirements.md
@@ -0,0 +1,12 @@
+Given a number of N integer array, print the Next Greater Element (NGE) for every element.
+
+Please use the appropriate data structure that helps you to minimize the code.
+
+Input: arr[] = [13,7,6,12]
+
+Output: 
+
+        13 -> -1
+        7  -> 12
+        6  -> 12
+        12 -> -1
