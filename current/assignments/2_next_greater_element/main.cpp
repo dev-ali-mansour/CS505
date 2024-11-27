75b6baa069361962cff5c828d77bd170cc821525
Ali Mansour <dev.ali.mansour@gmail.com>
2024-11-27 06:40:35 +0200

    refactor: Create Alternative Solutions For Assignment (2) Next Greater Element.
    Note: The alternative solutions give us the next greater element for each element but without keeping the order of the original list elements.

--- current/assignments/2_next_greater_element/main.cpp
+++ current/assignments/2_next_greater_element/main.cpp
@@ -3,20 +3,27 @@
 
 using namespace std;
 
-void findNextGreaterElement(const int arr[], int size);
+void findNGE(const int arr[], int size);
+
+void findNGE2(const int arr[], int size);
+
+void printNGE(int arr[], int n);
 
 int main() {
     int arr[] = {13, 7, 6, 12};
     int arrSize = sizeof(arr) / sizeof(arr[0]);
 
-    findNextGreaterElement(arr, arrSize);
-
+    findNGE(arr, arrSize);
+    cout << "=========" << endl;
+    findNGE2(arr, arrSize);
+    cout << "=========" << endl;
+    printNGE(arr, arrSize);
     return 0;
 }
 
-void findNextGreaterElement(const int arr[], int size) {
-    int results[size];
+void findNGE(const int arr[], int size) {
     stack<int> stk;
+    int results[size];
 
     for (int i = size - 1; i >= 0; --i) {
         while (!stk.empty() && stk.top() <= arr[i])
@@ -29,4 +36,35 @@ void findNextGreaterElement(const int arr[], int size) {
 
     for (int i = 0; i < size; ++i)
         cout << arr[i] << " -> " << results[i] << endl;
+}
+
+void findNGE2(const int arr[], int size) {
+    stack<int> stk;
+    int result;
+
+    for (int i = size - 1; i >= 0; --i) {
+        while (!stk.empty() && stk.top() <= arr[i])
+            stk.pop();
+
+        result = stk.empty() ? -1 : stk.top();
+        cout << arr[i] << " -> " << result << endl;
+
+        stk.push(arr[i]);
+    }
+}
+
+void printNGE(int arr[], int n) {
+    stack<int> s;
+    for (int i = 0; i < n; i++) {
+        while (!s.empty() && s.top() < arr[i]) {
+            cout << s.top() << " --> " << arr[i] << endl;
+            s.pop();
+        }
+        s.push(arr[i]);
+    }
+
+    while (!s.empty()) {
+        cout << s.top() << " --> " << -1 << endl;
+        s.pop();
+    }
 }
